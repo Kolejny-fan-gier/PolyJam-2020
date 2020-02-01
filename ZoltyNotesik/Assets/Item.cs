5b5b909bf638cbb3d653a705d50ec81d70102b29
Ortuy <Ortuy@users.noreply.github.com>
2020-02-01 20:08:04 +0100

    Itemki!!!
    Wszystkie itemy są w miarę ogarnięte

--- ZoltyNotesik/Assets/Item.cs
+++ ZoltyNotesik/Assets/Item.cs
@@ -51,6 +51,7 @@ public class Item : MonoBehaviour
                     interactingPlayer[0].carriesItem = true;
                     interactingPlayer[0].freeToPickup = false;
                     interactingPlayer[0].itemSprite.sprite = itemImage;
+                    interactingPlayer[0].itemStateSprite.sprite = stateSprite.sprite;
                     interactingPlayer[0].droppedItemActivator = activator;
                     gameObject.SetActive(false);
                 }
@@ -62,6 +63,7 @@ public class Item : MonoBehaviour
                     interactingPlayer[1].carriesItem = true;
                     interactingPlayer[1].freeToPickup = false;
                     interactingPlayer[1].itemSprite.sprite = itemImage;
+                    interactingPlayer[1].itemStateSprite.sprite = stateSprite.sprite;
                     interactingPlayer[1].droppedItemActivator = activator;
                     gameObject.SetActive(false);
                 }
